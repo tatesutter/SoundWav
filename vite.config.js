0783f2d71ad053303ab4d45c8709e0384b97637c
Tate Sutter <tatesutter@Tates-MacBook-Pro-2.local>
2025-03-25 20:15:03 -0500

    api fallback

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
-    react(),
-  ]
+    react()
+  ],
+  server: {
+    historyApiFallback: true
+  }
   
 });
